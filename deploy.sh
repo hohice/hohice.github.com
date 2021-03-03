699c7d3b0dd4d2d8b7ed8397cff0d56a299f960b
zhennan.hb <zhennan.hb@antfin.com>
2021-03-03 23:02:55 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cp -r docs/.vuepress/dist/* ./.
+
+# 如果发布到 https://<USERNAME>.github.io
+git add -A
+git commit -m 'deploy'
+git push
